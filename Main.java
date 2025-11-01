7bc8f09e0a3953cba40a80cf51c46f728c317609
Aditya-H2O <adityaharimuti@gmail.com>
2025-10-31 20:45:14 -0400

    Deleted First Call Invocation

--- Main.java
+++ Main.java
@@ -11,8 +11,6 @@ import java.util.ArrayList;
 
      public static void main(String[] args)
     {
-        System.out.println(call());
-
         final int NumberOfWValues = 1000; 
 
         ArrayList<Double> WValues = new ArrayList<>();
